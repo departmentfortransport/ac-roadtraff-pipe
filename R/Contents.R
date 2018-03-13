0b245cabc71acaccabb37f7241763923ac2e08c9
LUKE SHAW <Luke.Shaw@dft.gsi.gov.uk>
2018-03-13 19:07:28 +0000

    contents page
    Creating the function "contents_TRA25". There is a bug for col widths coming out wrong when running though?
    
    See difference between TRA2502_run_script (with Contents page but bad row/col widths) and
    TRA2501_run_script (no Contents page but formatted correctly

---
+++ R/Contents.R
@@ -0,0 +1,71 @@
+#############
+##Function for getting the Contents page with right dates on
+#############
+
+#' @title contents
+#' @description makes the contents page for TRA25 tables
+#' @param table_set
+#' @param UPDATE
+#' @examples
+#' @export
+contents_TRA25 <- function(table_set, year, quarter, save_to, save_over=F,
+                           last_updated="FILL IN", next_update="FILL IN"){
+  #Uses the Contents template and adds the right year, quarter to the table
+
+  if (!(table_set %in% c("TRA2501","TRA2502","TRA2503","TRA2504","TRA2505","TRA2506"))){
+    stop(paste("the table set", table_set, "does not exist"))
+  }
+
+  wb <- openxlsx::loadWorkbook(system.file(paste0(table_set,"_template.xlsx"), package="LStest"))
+
+
+  #Work out the text for the title of the contents page
+  if (quarter==1){
+    text <- paste("April", year - 1, "- March", year)
+  } else if(quarter==2) {
+    text <- paste("July", year - 1, "- June", year)
+  } else if(quarter==3) {
+  text <- paste("October", year - 1, "- September", year)
+  } else if(quarter==4) {
+    text <- paste("January", year, "- December", year)
+  }
+  table_title <- paste("Provisional Road Traffic Estimates, Great Britain:", text)
+
+
+
+
+  #Write title and last updated / next update
+  openxlsx::writeData(wb, "Contents", table_title, startCol = 2, startRow = 11)
+  openxlsx::writeData(wb, "Contents", last_updated, startCol = 5, startRow = 16)
+  openxlsx::writeData(wb, "Contents", next_update, startCol = 5, startRow = 17)
+
+
+
+  setwd(save_to) #where the output will be saved. Default is current folder
+  filename <- paste0(table_set,".xlsx")
+
+  if (file.exists(filename)) {
+    if(save_over){ #we'll remove the file that exists so we can save with identical name
+      file.remove(filename)}
+    else{
+      #add on the date and time of saving to file name so we're not overwriting
+      filename <- paste0(table_set, "_", gsub(" ","_",substr(Sys.time(),6,16)),".xlsx")
+      }
+  }
+
+  #Hide the gridlines and add the 2 images (DfT, National Stats)
+  openxlsx::showGridLines(wb, "Contents", showGridLines = FALSE)
+  unit <- 0.8 #can alter to scale how large you want the images
+  openxlsx::insertImage(wb, "Contents", system.file("DfT_logo.png", package = "LStest"),  height = 2*unit, width = 2*unit*1.5,
+                        startRow = 2,startCol = 2)
+  openxlsx::insertImage(wb, "Contents", system.file("nationalstats_logo.png", package = "LStest"),  height = unit, width = unit,
+                        startRow = 2,startCol = 7)
+
+  #Save the file
+  openxlsx::saveWorkbook(wb, filename)
+}
+
+#LStest:::contents_TRA25("TRA2502", 2017, 3, save_to = "/Users/Luke/Documents/table_dump/")
+
+
+
--- exec/TRA2502_run_script.R
+++ exec/TRA2502_run_script.R
@@ -1,6 +1,10 @@
 ########
-##Script that outputs TRA2501 as one doc
+##Script that outputs TRA2502 as one doc
 ##13/03/2018 work in progress
+
+##CHANGE LINE 9 TO YOUR DESIRED FOLDER THEN RUN ALL IN ONE GO BY PRESSING
+##"CTRL+ A" then "CTRL + R"
+########
 
 ##CHANGE LINE 9 TO YOUR DESIRED FOLDER THEN RUN ALL IN ONE GO BY PRESSING
 ##"CTRL+ A" then "CTRL + R"
@@ -13,17 +17,29 @@ save_loc <- "/Users/Luke/Documents/table_dump/"
 #and then it will be saved to where your current session is, which you can see
 #by running:
 #  getwd()
+year <- 2017
+quarter <- 3
+last_updated <- "November 2017"
+next_update <- "May 2018"
+#^^^^Update the year and quarter to most recent
+
 
 ###############################################
 ##RUN EVERYTHING BELOW THIS IN ONE
 ###############################################
 
-make_TRA2501 <- function(save_loc=getwd()){
+make_TRA2502 <- function(save_loc=getwd()){
 
   raw <- api_get_data()
 
-  footer_text <- c("Other = Two wheeled motor vehicles, buses, and coaches",
-                   "Note: Total column may not match sum due to rounding",
+  #Start by making the contents page
+  contents_TRA25("TRA2502", year = year, quarter = quarter,
+                 save_to = save_loc, save_over = TRUE,
+                 last_updated = last_updated,
+                 next_update = next_update)
+
+
+  footer_text <- c("Note: Total column may not match sum due to rounding",
                    "(1) Figures affected by September 2000 fuel protest",
                    "(2) 2001 figures affected by the impact of Foot and Mouth disease",
                    "(3) Affected by heavy snowfall",
@@ -36,66 +52,66 @@ make_TRA2501 <- function(save_loc=getwd()){
                    "Last updated: November 2017",
                    "Next update: May 2018")
 
-  ###TRA2501a####
-  new_data <- raw2new(raw, roll=T, type="vehicle", units="traffic", km_or_miles = "miles")
+  ###TRA2502a####
+  new_data <- raw2new(raw, roll=T, type="road", units="traffic", km_or_miles = "miles")
   title_text <- c("Department for Transport statistics",
                   "Traffic",
-                  "Table TRA2501a",
-                  "Road traffic (vehicle miles) by vehicle type in Great Britain, rolling annual totals from 1994",
-                  "Billion vehicle miles (not seasonally adjusted)",
-                  "Rolling annual totals")
+                  "Table TRA2502a",
+                  "Road traffic (vehicle miles) by road class in Great Britain, rolling annual totals from 1994",
+                  "",
+                  "Billion vehicle miles (not seasonally adjusted)")
   new2xl(new_data,
          title_text,
          footer_text,
-         table_name = "TRA2501a",
+         table_name = "TRA2502a",
          save_to = save_loc,
-         start_from_wb = "TRA2501.xlsx",
+         start_from_wb = "TRA2502.xlsx",
          save_over = TRUE)
 
-  ###TRA2501b####
-  new_data <- raw2new(raw, roll=T, type="vehicle", units="index")
+  ###TRA2502b####
+  new_data <- raw2new(raw, roll=T, type="road", units="index")
   title_text <- c("Department for Transport statistics",
                   "Traffic",
-                  "Table TRA2501b",
-                  "Road traffic (vehicle miles) by vehicle type in Great Britain, rolling annual totals from 1994",
+                  "Table TRA2502b",
+                  "Road traffic (vehicle miles) by road type in Great Britain, rolling annual totals from 1994",
                   "",
                   "Index numbers (Q4 1994 = 100)")
   new2xl(new_data,
          title_text,
          footer_text,
-         table_name = "TRA2501b",
+         table_name = "TRA2502b",
          save_to = save_loc,
-         start_from_wb = "TRA2501.xlsx",
+         start_from_wb = "TRA2502.xlsx",
          save_over = TRUE)
 
 
   ####TRA2501c####
-  new_data <- raw2new(raw, roll=T, type="vehicle", units="percentage")
+  new_data <- raw2new(raw, roll=T, type="road", units="percentage")
   title_text <- c("Department for Transport statistics",
                   "Traffic",
-                  "Table TRA2501c",
-                  "Road traffic (vehicle miles) by vehicle type in Great Britain, rolling annual totals from 1994",
+                  "Table TRA2502c",
+                  "Road traffic (vehicle miles) by road type in Great Britain, rolling annual totals from 1994",
                   "",
                   "Percentage change on previous year")
 
   new2xl(new_data,
          title_text,
          footer_text,
-         table_name = "TRA2501c",
+         table_name = "TRA2502c",
          save_to = save_loc,
-         start_from_wb = "TRA2501.xlsx",
+         start_from_wb = "TRA2502.xlsx",
          save_over = TRUE)
 
   ####TRA2501d####
   #waiting on seasonal data
 
   ####TRA2501e####
-  new_data <- raw2new(raw, roll=F, type="vehicle", units="traffic", km_or_miles = "miles")
+  new_data <- raw2new(raw, roll=F, type="road", units="traffic", km_or_miles = "miles")
   #title and footer
   title_text <- c("Department for Transport statistics",
                   "Traffic",
-                  "Table TRA2501e",
-                  "Road traffic (vehicle miles) by vehicle type in Great Britain, quarterly from 1994",
+                  "Table TRA2502e",
+                  "Road traffic (vehicle miles) by road type in Great Britain, quarterly from 1994",
                   "",
                   "Billion vehicle miles (not seasonally adjusted)")
 
@@ -103,9 +119,9 @@ make_TRA2501 <- function(save_loc=getwd()){
   new2xl(new_data,
          title_text,
          footer_text,
-         table_name = "TRA2501e",
+         table_name = "TRA2502e",
          save_to = save_loc,
-         start_from_wb = "TRA2501.xlsx",
+         start_from_wb = "TRA2502.xlsx",
          save_over = TRUE)
 
 
@@ -116,4 +132,4 @@ make_TRA2501 <- function(save_loc=getwd()){
   #waiting on seasonal data
 }
 
-make_TRA2501(save_loc)
+make_TRA2502(save_loc)
